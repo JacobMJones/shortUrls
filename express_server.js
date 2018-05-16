a5d19eec3f01fdad1ae630d04b822407cf7ef5ca
JacobMJones <theretherehuman@gmail.com>
2018-05-16 17:03:02 +0000

    done updating urls

--- express_server.js
+++ express_server.js
@@ -29,7 +29,7 @@ console.log('in edit', id, url);
   
   if (url) {
     console.log('in change url');
-     urlDatabase.id = req.body.longURL;
+     urlDatabase[id] = req.body.longURL;
      res.redirect("/urls")
   } else {
     console.log('NOT in change url');
